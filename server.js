ec37b9f43d3e6d52770aaf194228572eaf0b938e
rnml6 <andayaronmel@gmail.com>
2026-01-03 08:36:16 +0800

    backend

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ import cors from 'cors';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
